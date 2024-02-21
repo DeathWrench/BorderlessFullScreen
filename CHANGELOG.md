71438dea7a11ca72488d670976feef3892e1702e
DeathWrench <45341450+DeathWrench@users.noreply.github.com>
2024-02-21 07:25:18 -0500

    Add files via upload

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# 0.0.2
+- Fixed readme.
+- Added Return
 
 # 0.0.1
 - Release
--- Plugin.cs
+++ Plugin.cs
@@ -5,14 +5,14 @@ using System.Reflection;
 
 namespace BorderlessFullScreen
 {
-    [BepInPlugin("BorderlessFullScreen", "BorderlessFullScreen", "0.0.1")]
+    [BepInPlugin("BorderlessFullScreen", "BorderlessFullScreen", "0.0.2")]
     [HarmonyPatch]
     public class BorderlessFullScreen : BaseUnityPlugin
     {
         void Start()
         {
             var ahk = AutoHotkeyEngine.Instance; 
-            ahk.ExecRaw("SetTitleMatchMode, RegEx\r\nWinActivate, Lethal Company, , BepInEx 5.4.21.0 - Lethal Company\r\nWinActivate, Lethal Company, , BepInEx 5.4.22.0 - Lethal Company\r\nWinGet, WindowID, ID, A\r\nWinSet, Style, -0xC40000, ahk_id %WindowID%\r\nWinMove, ahk_id %WindowID%, , 0, 0, A_ScreenWidth, A_ScreenHeight");
+            ahk.ExecRaw("SetTitleMatchMode, RegEx\r\nWinActivate, Lethal Company, , BepInEx 5.4.21.0 - Lethal Company\r\nWinActivate, Lethal Company, , BepInEx 5.4.22.0 - Lethal Company\r\nWinGet, WindowID, ID, A\r\nWinSet, Style, -0xC40000, ahk_id %WindowID%\r\nWinMove, ahk_id %WindowID%, , 0, 0, A_ScreenWidth, A_ScreenHeight\r\nReturn");
             Logger.LogInfo($"Plugin {"BorderlessFullScreen"} is loaded!");
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
         }

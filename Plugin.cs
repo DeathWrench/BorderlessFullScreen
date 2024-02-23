904941b1f07763399085fefc6e6b83e514f1b362
DeathWrench <45341450+DeathWrench@users.noreply.github.com>
2024-02-23 07:09:08 -0500

    0.0.9

--- Plugin.cs
+++ Plugin.cs
@@ -2,6 +2,8 @@ using AutoHotkey.Interop;
 using BepInEx;
 using HarmonyLib;
 using System.Reflection;
+using UnityEngine;
+using UnityEngine.Video;
 
 namespace BorderlessFullScreen
 {
@@ -15,6 +17,12 @@ namespace BorderlessFullScreen
             ahk.ExecRaw("procName := \"UnityWndClass\"\r\nWinGet Style, Style, % \"ahk_class \" procName\r\nIf (Style & 0xC40000)\r\n{\r\nWinSet, Style, -0xC40000, % \"ahk_class \" procName\r\nWinMove, % \"ahk_class \" procName, , 0, 0, A_ScreenWidth + 1, A_ScreenHeight + 1\r\nWinMove, % \"ahk_class \" procName, , 0, 0, A_ScreenWidth, A_ScreenHeight\r\n}\r\nReturn");
             Logger.LogInfo($"Plugin {"BorderlessFullScreen"} is loaded!");
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+        [HarmonyPatch(typeof(IngamePlayerSettings), "SetFullscreenMode")]
+        [HarmonyPrefix]
+        private static void SetFullscreenMode(int value)
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
         }
     }
 }
